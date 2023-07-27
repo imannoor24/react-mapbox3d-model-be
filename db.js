2ee9cf869c09e5ac806255c69447764906ec60fc
Iman Noor <imannoor.kr2016@gmail.com>
2023-07-27 13:37:26 +0500

    db file updated

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool ({
-    user: "postgres",
-    host: "localhost",
-    database:"pakstats",
-    password: "iman2404",
-    port:5432,
+    user: "enter_your_username",
+    host: "localhost", //enter your host name here
+    database:"enter_yourdb_name",
+    password: "enter_your_password",
+    port:5432, //or any other port incase 
 });
 
 
